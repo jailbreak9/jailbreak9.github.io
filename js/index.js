89d87f8525c5c6eab3b6558797999cdbb8808905
jailbreak9 <95508711+jailbreak9@users.noreply.github.com>
2021-12-04 13:13:59 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -29,8 +29,8 @@ async function pwnMe() {
 		  return;
 		}
 		
-		if (currentFirmware(navigator.userAgent).startsWith("14.3")) {
-		  socket.emit("log_normal", "Starting exploitation for iOS 14.3");
+		if (currentFirmware(navigator.userAgent).startsWith("14.5")) {
+		  socket.emit("log_normal", "Starting exploitation for iOS 14.5");
 		  await kickstart146();
 		  return;
 		}
